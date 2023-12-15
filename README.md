43a475c5ab875324a42d246f3516ae157eacb611
qblone <q.b.lone@tudelft.nl>
2023-12-15 18:04:52 +0100

    added functionality to unique the ixp names and remove subnets

--- README.md
+++ README.md
@@ -24,14 +24,24 @@ This repository contains Python scripts to fetch, merge, and analyze Internet Ex
    - Outputs the final merged data to `data/ixps.json`.
 
 ## Merging Logic
-The merge_ixp_data.py script follows a systematic approach to merge data from different sources while avoiding duplicates based on the peeringlan field. Here's a step-by-step breakdown:
+The merge_ixp_data.py script processes data from different sources related to internet exchange points (IXPs) and merges them into a unified dataset. The data includes information about regions, cities, countries, and peering LAN prefixes associated with various IXPs. The script uses the `ipaddress` module to handle IPv4 and IPv6 prefixes.
+
+The `process_data_file` function reads data from JSON files and populates a dictionary (`ip_dict`) with information about each IXP entry, including the peering LAN prefix. It checks for duplicate prefixes and handles cases where a prefix is a subnet or supernet of an existing entry. If a supernet is found, it removes subnets of that supernet from the dictionary to ensure a consistent and non-overlapping dataset.
+
+The script also ensures that each entry has a unique identifier (`id`) by combining the IXP's identifier (pdb, pch, or he), the IX ID, and a counter. The generated ID format is "{pdb/pch/he}_{ix_id}_{counter:02}", where the counter ensures uniqueness. The `create_entry_data` function is responsible for constructing the entry data with the modified ID and prefix.
+
+Finally, the script standardizes region names, and the resulting dataset is saved to a JSON file named `ixps.json`. The `standardize_region_name` function helps in converting specific region names to a standardized format.
+
+In summary the merge_ixp_data.py script follows following approach to merge data from different sources while avoiding duplicates based on the peeringlan field. 
+
+Here's a step-by-step breakdown:
 1. Load data from PeeringDB, creating a dictionary based on the `peeringlan` field for uniqueness.
 2. Load data from PCH, adding entries that are missing in PeeringDB.
 3. Load data from Hurricane Electric, adding entries that are missing in both PeeringDB and PCH.
-4. Standardize region names for consistency.
-5. Save the final merged data to `data/ixps.json`.
+4. Remove duplicates and subnets
+5. Standardize region names for consistency.
+6. Save the final merged data to `data/ixps.json`.
 
-This process ensures a comprehensive and non-redundant dataset from multiple sources, avoiding duplicates based on the `peeringlan` field.
 
 ## Instructions
 
--- merge_ixp_data.py
+++ merge_ixp_data.py
@@ -1,4 +1,6 @@
+from pprint import pprint
 import json
+import ipaddress
 
 def standardize_region_name(region):
     if region == "Asia-Pacific":
@@ -8,40 +10,90 @@ def standardize_region_name(region):
     else:
         return region  # Leave other regions unchanged
 
+def create_entry_data(entry, prefix, suffix=''):
+    return {
+        'entry_data': {
+            **entry,
+            'id': f"{entry['id']}_{suffix:02}" if suffix else entry['id']
+        },
+        'prefix': prefix,
+        'id': f"{entry['id']}_{suffix:02}" if suffix else entry['id'],
+    }
+
+def is_subnet(existing, new_prefix):
+    if existing.version != new_prefix.version:
+        return False
+    return existing.subnet_of(new_prefix)
+
+def is_supernet(existing, new_prefix):
+    if existing.version != new_prefix.version:
+        return False
+    return existing.supernet_of(new_prefix)
+
+def remove_entry_with_prefix(prefix_to_remove, ip_dict):
+    # Create a list of keys to remove
+    keys_to_remove = [key for key, entry_data in ip_dict.items() if entry_data['prefix'] == prefix_to_remove]
+
+    # Remove the entries with the specified prefix
+    for key in keys_to_remove:
+        del ip_dict[key]
+
+    if keys_to_remove:
+        print(f"Entries with prefix {prefix_to_remove} removed.")
+    else:
+        print(f"No entries found with prefix {prefix_to_remove}.")
+
+def process_data_file(file_path, ip_dict):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+
+        for entry in data:
+            peeringlan = entry['peeringlan']
+            prefix = ipaddress.IPv4Network(peeringlan, strict=False) if ':' not in peeringlan else ipaddress.IPv6Network(peeringlan, strict=False)
+
+            # Check if the prefix already exists in the dictionary, if yes, skip the entry
+            prefix_exists = any(ip_dict_entry['prefix'] == prefix for ip_dict_entry in ip_dict.values())
+            if prefix_exists:
+                continue
+
+            # Check if it's a subnet of an existing prefix
+            subnet_of_existing = any(is_subnet(prefix, ip_dict_entry['prefix']) for ip_dict_entry in ip_dict.values())
+            if subnet_of_existing:
+                continue
+
+            # Check if it's a supernet of an existing prefix
+            supernet_of_existing = any(is_supernet(prefix, ip_dict_entry['prefix']) for ip_dict_entry in ip_dict.values())
+            if supernet_of_existing:
+                # Remove subnets of the existing supernet from the dictionary
+                subnets_to_remove = [ip_dict_entry['prefix'] for ip_dict_entry in ip_dict.values() if is_subnet(ip_dict_entry['prefix'], prefix)]
+                for subnet_to_remove in subnets_to_remove:
+                    remove_entry_with_prefix(subnet_to_remove, ip_dict)
+
+            # Add the entry to the dictionary with a unique ID
+            counter = 1
+            while f"{entry['id']}_{counter:02}" in ip_dict:
+                counter += 1
+            ip_dict[f"{entry['id']}_{counter:02}"] = create_entry_data(entry, prefix, counter)
+
 def merge_ixp_data():
-    # Load data from peeringdb_peeringlan.json
-    with open('data/peeringdb_peeringlan.json', 'r') as file:
-        peeringdb_data = json.load(file)
+    # Separate dictionary for IPv4 and IPv6 entries
+    ip_dict = {}
 
-    # Create a dictionary of unique entries based on the peeringlan field
-    peeringdb_dict = {entry['peeringlan']: entry for entry in peeringdb_data}
+    # Process peeringdb_data
+    process_data_file('data/peeringdb_peeringlan.json', ip_dict)
 
-    # Load data from pch.json
-    with open('data/pch.json', 'r') as file:
-        pch_data = json.load(file)
+    # Process pch_data
+    process_data_file('data/pch.json', ip_dict)
 
-    # Merge only the entries that are missing in peeringdb
-    for entry in pch_data:
-        peeringlan = entry['peeringlan']
-        if peeringlan not in peeringdb_dict:
-            peeringdb_dict[peeringlan] = entry
-
-    # Load data from he.json
-    with open('data/he.json', 'r') as file:
-        he_data = json.load(file)
-
-    # Merge only the entries that are missing in both peeringdb and pch
-    for entry in he_data:
-        peeringlan = entry['peeringlan']
-        if peeringlan not in peeringdb_dict:
-            peeringdb_dict[peeringlan] = entry
+    # Process he_data
+    process_data_file('data/he.json', ip_dict)
 
     # Convert the merged dictionary values to a list for the final output
-    merged_result = list(peeringdb_dict.values())
+    merged_result = [entry_data['entry_data'] for entry_data in ip_dict.values()]
 
     # Standardize region names
-    for entry in merged_result:
-        entry['region'] = standardize_region_name(entry.get('region', ''))
+    for entry_data in merged_result:
+        entry_data['region'] = standardize_region_name(entry_data.get('region', ''))
 
     # Save the final output in ixps.json
     with open('data/ixps.json', 'w') as file:
@@ -49,4 +101,3 @@ def merge_ixp_data():
 
 if __name__ == '__main__':
     merge_ixp_data()
-
